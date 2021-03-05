1a4bfebd6682cabfaa37d7dd568bcc7f241951f2
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-03-04 22:55:21 -0500

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -6,18 +6,23 @@ letters=(a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9
 function whoischeck {
 
 if [ "${domain:0:1}" != "-" ] && [ "${domain:3:1}" != "-" ]; then
-#     if [ "${domain:1:1}" = "-" ] && [ "${domain:2:1}" = "-" ]; then
-        isopen=$(whois "$1")
-        ischeck=$(echo "$isopen" | grep 'No match for domain')
-        if [ "$ischeck" ]; then
-                echo "$ischeck" >> log.log
-                echo "$ischeck" >> open.log
+        digging=$(dig @8.8.8.8 "$domain" any +short | head -n1)
+        if [ "$digging" = "" ]; then
+                isopen=$(whois "$1")
+                ischeck=$(echo "$isopen" | grep 'No match for domain')
+                if [ "$ischeck" ]; then
+                        echo "$ischeck" >> alldomains.log
+                        echo "$ischeck" >> opendomains.log
+                else
+                        isopen=$(echo "$isopen" | grep 'Registry Expiry Date' |  awk '{ print $1" "$2" "$3" "$4 }' )
+                        echo "$isopen - $qw$er$ty$ui" >> alldomains.log
+                fi
+                sleep 5
         else
-                isopen=$(echo "$isopen" | grep 'Registry Expiry Date' |  awk '{ print $1" "$2" "$3" "$4 }' )
-                echo "$isopen - $qw$er$ty$ui" >> log.log
+                echo "$digging : - $qw$er$ty$ui" >> alldomains.log
+                sleep 0.5
         fi
-        sleep 10
-#    fi
+
 fi
 }
 for qw in "${letters[@]}"; do
@@ -30,3 +35,4 @@ for qw in "${letters[@]}"; do
                  done
          done
 done
+
